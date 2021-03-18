fefd546d96b44d560707d95ccc5cc66150033cf6
jaussems <jannes2908@hotmail.com>
2021-03-18 11:57:40 +0100

    ring without the nodes ofcourse

---
+++ index.js
@@ -0,0 +1,9 @@
+const toTitleCase = require('to-title-case');
+
+// console.log(TitleCase);
+
+const lower = 'the lord of the rings: the return of the king';
+
+const title = toTitleCase(lower);
+
+console.log(title);
